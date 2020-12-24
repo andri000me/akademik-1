9288da8997d594afeccc8c883bfc2e27cd6fa37d
alifananda <alif.ananda33@gmail.com>
2020-12-24 19:01:45 +0700

    benerin nilai raport

--- application/views/raport/list_siswa.php
+++ application/views/raport/list_siswa.php
@@ -3,8 +3,8 @@
     <table class="table table-bordered">
         <tr><td width="200">TAHUN AKADEMIK</td><td> : <?php echo get_tahun_akademik_aktif('tahun_akademik')?></td></tr>
         <tr><td>SEMESTER</td><td> :  <?php echo get_tahun_akademik_aktif('semester_aktif')?></td></tr>
-        <tr><td>JURUSAN</td><td> : KELAS <?php echo $rombel['kelas'].' '.$rombel['nama_jurusan']?> ( <?php echo $rombel['nama_rombel']?> )</td></tr>
-       
+        <!-- <tr><td>JURUSAN</td><td> : KELAS <?php echo $rombel['kelas'].' '.$rombel['nama_jurusan']?> ( <?php echo $rombel['nama_rombel']?> )</td></tr> -->
+
     </table>
     <!-- end: DYNAMIC TABLE PANEL -->
 </div>
@@ -24,12 +24,12 @@
                 <table class="table table-bordered">
                     <tr><th>NIM</th><th>NAMA</th><th>LIHAT NILAI</th></tr>
                     <?php
-                    foreach ($siswa->result() as $row){
-                        echo "<tr>
-                            <td width='100'>$row->nim</td>
-                            <td>$row->nama</td>
-                            <td width='100'>".anchor('raport/nilai_semester/'.$row->nim,'Lihat Raport','class="btn btn-success btn-sm"')."</td></tr>";
-                    }
+                    // foreach ($siswa->result() as $row){
+                    //     echo "<tr>
+                    //         <td width='100'>$row->nim</td>
+                    //         <td>$row->nama</td>
+                    //         <td width='100'>".anchor('raport/nilai_semester/'.$row->nim,'Lihat Raport','class="btn btn-success btn-sm"')."</td></tr>";
+                    // }
                     ?>
                 </table>
         </div>
