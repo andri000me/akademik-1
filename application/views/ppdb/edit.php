90108c532f178c6365198c186100ace9f21b5a81
alifananda <alif.ananda33@gmail.com>
2020-12-27 21:26:49 +0700

    edit ppdbne ketinggalan

---
+++ application/views/ppdb/edit.php
@@ -0,0 +1,111 @@
+<div class="col-sm-12">
+    <!-- start: TEXT FIELDS PANEL -->
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <i class="fa fa-external-link-square"></i>
+            Edit Data Siswa Pendaftar
+            <!-- <div class="panel-tools">
+                <a class="btn btn-xs btn-link panel-collapse collapses" href="#">
+                </a>
+                <a class="btn btn-xs btn-link panel-config" href="#panel-config" data-toggle="modal">
+                    <i class="fa fa-wrench"></i>
+                </a>
+                <a class="btn btn-xs btn-link panel-refresh" href="#">
+                    <i class="fa fa-refresh"></i>
+                </a>
+                <a class="btn btn-xs btn-link panel-expand" href="#">
+                    <i class="fa fa-resize-full"></i>
+                </a>
+                <a class="btn btn-xs btn-link panel-close" href="#">
+                    <i class="fa fa-times"></i>
+                </a> -->
+            </div>
+        </div>        <div class="panel-body">
+
+                    <?php
+                    echo form_open_multipart('ppdb/add', 'role="form" class="form-horizontal"');
+                    echo form_hidden('nisn', $siswa['nisn']);
+                    ?>
+
+                    <div class="form-group">
+                        <label class="col-sm-3 control-label" for="form-field-1">
+                            NISN
+                        </label>
+                        <div class="col-sm-5">
+                            <input type="text" value="<?php echo $siswa['nisn'] ?>" name="nisn" placeholder="MASUKAN NISN" id="form-field-1" class="form-control">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-sm-3 control-label" for="form-field-1">
+                            NAMA
+                        </label>
+                        <div class="col-sm-9">
+                            <input type="text" value="<?php echo $siswa['nama'] ?>" name="nama" placeholder="MASUKAN NAMA LENGKAP" id="form-field-1" class="form-control">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-sm-3 control-label" for="form-field-1">
+                            TTL
+                        </label>
+                        <div class="col-sm-5">
+                            <input type="text" value="<?php echo $siswa['tempat_lahir'] ?>" name="tempat_lahir" placeholder="TEMPAT LAHIR" id="form-field-1" class="form-control">
+                        </div>
+                        <div class="col-sm-4">
+                            <input type="date" value="<?php echo $siswa['tanggal_lahir'] ?>" name="tanggal_lahir" placeholder="TANGGAL LAHIR" id="form-field-1" class="form-control">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-sm-3 control-label" for="form-field-1">
+                            JENIS KELAMIN
+                        </label>
+                        <div class="col-sm-5">
+                            <?php
+                            echo form_dropdown('gender', array('P' => 'LAKI LAKI', 'W' => 'PEREMPUAN'), $siswa['gender'], "class='form-control'");
+                            ?>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-sm-3 control-label" for="form-field-1">
+                            AGAMA
+                        </label>
+                        <div class="col-sm-5">
+                            <?php
+                            echo cmb_dinamis('agama', 'tbl_agama', 'nama_agama', 'kd_agama', $siswa['kd_agama']);
+                            ?>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-sm-3 control-label" for="form-field-1">
+                            Alamat
+                        </label>
+                        <div class="col-sm-6">
+                            <textarea name="alamat" value="<?php echo $siswa['alamat'] ?>" class="form-control"></textarea>
+                        </div>
+
+                    </div>
+                    <div class="form-group">
+                        <label class="col-sm-3 control-label" for="form-field-1">
+                            Foto
+                        </label>
+                        <div class="col-sm-2">
+                            <input type="file" name="userfile">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label" for="form-field-1">
+
+                        </label>
+                        <div class="col-sm-2">
+                            <button type="submit" name="submit" class="btn btn-danger  btn-sm">SIMPAN</button>
+                        </div>
+                        <div class="col-sm-3">
+                            <?php echo anchor('ppdb', 'Kembali', array('class' => 'btn btn-info btn-sm')); ?>
+                        </div>
+                    </div>
+                    </form>
+                </div>
+              </div>
+              <!-- end: TEXT FIELDS PANEL -->
+          </div>
